163e5df0e70d139395300a725000a2ff7b3cbba5
James Chow <dragonmaster510@gmail.com>
2016-05-01 17:11:42 -0400

    Committing dbhandler.

---
+++ app/src/main/java/com/ubi/alonso/givemesomespace/DBHandler.java
@@ -0,0 +1,117 @@
+package com.ubi.alonso.givemesomespace;
+
+
+import android.util.Log;
+
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+import com.firebase.client.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by jameschow on 4/29/16.
+ */
+public class DBHandler {
+    private static int ccAvg;
+    private static int libAvg;
+    private Firebase fb;
+    public DBHandler(Firebase firebase) {
+        this.fb = firebase;
+    }
+
+    public void sendData(BuildingData data) {
+        try {
+
+            Firebase newref =  fb.child("inputs").push();
+
+            Firebase newData = newref.child("Building Name");
+            Firebase childrenTime = newref.child("Time");
+            Firebase childrenRate = newref.child("Rating");
+            newData.setValue(data.getBuildingName());
+            childrenTime.setValue(data.getTimeStamp());
+            childrenRate.setValue(data.getRating());
+
+        } catch (Exception e) {
+            Log.d("MESSAGE", e.toString());
+            e.printStackTrace();
+        }
+
+        Log.d("MESSAGE", "DATA SUCCESSFULLY SAVED");
+
+    }
+
+    public void retrieveData () {
+        final List<BuildingData> dataList = new ArrayList<BuildingData>();
+        this.fb.child("inputs").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                int counter = 0;
+                int limit;
+                if (Integer.parseInt(Long.toString(snapshot.getChildrenCount())) < 10) {
+                    limit = Integer.parseInt(Long.toString(snapshot.getChildrenCount()));
+                } else {
+                    limit = 10;
+                }
+                Log.d("MESSAGE","There are " + snapshot.getChildrenCount() + " blog posts");
+                for (DataSnapshot postSnapshot: snapshot.getChildren()) {
+                    try {
+//                        BuildingData post = (BuildingData) postSnapshot.getValue(BuildingData.class);
+//                        dataList.add(post);
+                        if (counter < limit-1) {
+                            String name = (String) postSnapshot.child("Building Name").getValue();
+                            long rating = (long) postSnapshot.child("Rating").getValue();
+                            long time = Long.parseLong(((String)postSnapshot.child("Time").getValue()));
+                            Log.d("MESSAGE", "The name is :"+name+" with rating "+rating+" at time "+time + " counter "+counter);
+                            dataList.add(new BuildingData(time,name,rating));
+                            counter++;
+
+                        } else if (counter == limit-1){
+                            String name = (String) postSnapshot.child("Building Name").getValue();
+                            long rating = (long) postSnapshot.child("Rating").getValue();
+                            long time = Long.parseLong(((String)postSnapshot.child("Time").getValue()));
+                            Log.d("MESSAGE", "The name is :"+name+" with rating "+rating+" at time "+time + " counter "+counter);
+                            dataList.add(new BuildingData(time,name,rating));
+                            Log.d("MESSAGE", "LIST IS "+dataList.toString());
+                            libAvg = computeAverageRate(dataList);
+                            Log.d("MESSAGE", "average is :"+ libAvg);
+                            break;
+                        } else {
+                            Log.d("MESSAGE", "LIST IS "+dataList.toString());
+                            libAvg = computeAverageRate(dataList);
+                            Log.d("MESSAGE", "average is :"+ libAvg);
+                            break;
+                        }
+                    } catch (Exception e) {
+                        Log.d("MESSAGE", e.toString());
+                        e.printStackTrace();
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                System.out.println("The read failed: " + firebaseError.getMessage());
+            }
+        });
+        Log.d("MESSAGE", "LIST IS "+dataList.toString());
+        Log.d("MESSAGE", "average is :"+this.computeAverageRate(dataList));
+    }
+
+    public int computeAverageRate (List<BuildingData> dataList) {
+        double sum = 0.0;
+        int avg;
+        int count = 0;
+        for (BuildingData d : dataList) {
+            sum += (double) (d.getRating());
+            count++;
+        }
+        avg = (int) (sum/count);
+        return avg;
+    }
+}
