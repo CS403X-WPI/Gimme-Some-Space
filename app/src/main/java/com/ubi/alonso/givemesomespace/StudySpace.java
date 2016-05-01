20d18455a5c5b408d77a3d37f6349e1f261f6d89
Benjamin Bianchi <bgbianchi@wpi.edu>
2016-04-30 20:00:18 -0400

    Addded Study Spaces and Custom Adapters.. everything we need for the activity.

---
+++ app/src/main/java/com/ubi/alonso/givemesomespace/StudySpace.java
@@ -0,0 +1,15 @@
+package com.ubi.alonso.givemesomespace;
+
+/**
+ * Created by ben on 4/30/16.
+ */
+public class StudySpace {
+    public String name;
+    public Integer rating;
+
+    public StudySpace(String name, int rating)
+    {
+        this.name = name;
+        this.rating = rating;
+    }
+}
---
+++ app/src/main/java/com/ubi/alonso/givemesomespace/studySpaceAdapters.java
@@ -0,0 +1,41 @@
+package com.ubi.alonso.givemesomespace;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.ubi.alonso.givemesomespace.StudySpace;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ben on 4/30/16.
+ */
+class StudySpaceAdapter extends ArrayAdapter<StudySpace> {
+    public StudySpaceAdapter(Context context, ArrayList<StudySpace> studyspaces)
+    {
+        super(context,0,studyspaces);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        StudySpace studySpace = getItem(position);
+
+        if (convertView == null)
+        {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_studyspace,parent,false);
+        }
+
+        TextView tvName = (TextView) convertView.findViewById(R.id.item_space_name_text);
+        TextView tvRating = (TextView) convertView.findViewById(R.id.item_space_name_rating);
+
+        tvName.setText(studySpace.name);
+        tvRating.setText(studySpace.rating);
+
+        return convertView;
+    }
+}
